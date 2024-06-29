43a8d802fc363388e168d1dd3986ca82b7855ea8
Rohan Nandan <rohannandan2004@gmail.com>
2024-06-30 06:04:36 +1200

    Feat: Change Site Title

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'TECH';
+export const SITE_TITLE = 'SRVSRR';
 export const SITE_DESCIPTION = 'Film Blogs, Reviews and News';
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
