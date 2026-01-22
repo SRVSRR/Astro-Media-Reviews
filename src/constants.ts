165afd53c97fd847fe48132ef405addabe98022a
Rohan Nandan <rohannandan2004@gmail.com>
2026-01-22 19:24:46 +1200

    Feat: SWE Revamp

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'SRVSRR';
-export const SITE_DESCIPTION = 'Film Blogs, Reviews and News';
+export const SITE_DESCIPTION = 'Software Development, Project Management & Tech Insights';
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
