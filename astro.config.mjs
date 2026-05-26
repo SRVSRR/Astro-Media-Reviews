85e0f722a95b8c3d9539dc166bc360ebafcc2c21
Rohan Nandan <rohannandan2004@gmail.com>
2026-05-27 10:27:29 +1200

    Feat: Change to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   site: "https://srvsrr.vercel.app",
   trailingSlash: "never",
   integrations: [tailwind(), sitemap()],
-  output: 'hybrid', // Pre-renders by default, keeps API dynamic
+  output: 'static', // Pre-renders by default, keeps API dynamic
   adapter: vercel({
     webAnalytics: {
       enabled: true // Keep it enabled if needed, or set it to false if not
